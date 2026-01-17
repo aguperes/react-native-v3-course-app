4e50da0fa884818618bdbdb8d2a2ad1a784726d7
Agustin Peres <agu.peres3@gmail.com>
2026-01-17 17:44:44 -0300

    Add modal and bottom tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,38 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { theme } from "../theme";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Shopping List" }} />
-    </Stack>
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping List",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="unordered-list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="clock-circle" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="lightbulb" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
