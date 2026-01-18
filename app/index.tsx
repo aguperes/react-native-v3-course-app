fb040068e954b632e885da0311a39ab3fea2d0e8
Agustin Peres <agu.peres3@gmail.com>
2026-01-18 16:19:07 -0300

    Add ScrollView

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,48 @@
-import { StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, TextInput, ScrollView } from "react-native";
 import { theme } from "../theme";
 import ShoppingListItem from "../components/ShoppingListItem";
+import { nanoid } from "nanoid/non-secure";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+};
 
 export default function App() {
+  const [value, setValue] = useState("");
+  const [shoppingList, setShoppingList] = useState<ShoppingListItemType[]>([]);
+
+  const handleOnSubmit = () => {
+    if (value.trim().length === 0) return;
+
+    const newItem = {
+      id: nanoid(),
+      name: value,
+    };
+    setShoppingList((prev) => [newItem, ...prev]);
+    setValue("");
+  };
+
   return (
-    <View style={styles.container}>
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" />
-      <ShoppingListItem name="Sugar" />
-    </View>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      stickyHeaderIndices={[0]}
+    >
+      <TextInput
+        placeholder="E.g. Coffee"
+        style={styles.textInput}
+        returnKeyType="done"
+        onChangeText={setValue}
+        onSubmitEditing={handleOnSubmit}
+        value={value}
+      />
+      {shoppingList &&
+        shoppingList.map(({ id, name }) => (
+          <ShoppingListItem key={id} name={name} />
+        ))}
+    </ScrollView>
   );
 }
 
@@ -16,6 +50,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
+    padding: 12,
+  },
+  contentContainer: {
+    paddingBottom: 24,
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    borderRadius: 50,
+    padding: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    marginHorizontal: 6,
+    backgroundColor: theme.colorWhite,
   },
 });
